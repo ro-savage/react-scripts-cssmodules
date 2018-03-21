08d4d9eca40f5c4217630d716a4e63c5bac7e361
Ro Savage <rowan.savage@gmail.com>
2018-03-21 23:09:51 +1100

    Update to v1.1.1

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -298,7 +298,7 @@ module.exports = {
                 extractTextPluginCSSOptions
               )
             ),
-            // Note: this won't work without `ExtractTextPluginCSS` in `plugins`.
+            // Note: this won't work without `new ExtractTextPlugin()` in `plugins`.
           },
           // "file" loader makes sure assets end up in the `build` folder.
           // When you `import` an asset, you get its filename.
@@ -310,7 +310,7 @@ module.exports = {
             // it's runtime that would otherwise processed through "file" loader.
             // Also exclude `html` and `json` extensions so they get processed
             // by webpacks internal loaders.
-            exclude: [/\.js$/, /\.html$/, /\.json$/],
+            exclude: [/\.(js|jsx|mjs)$/, /\.html$/, /\.json$/],
             options: {
               name: 'static/media/[name].[hash:8].[ext]',
             },

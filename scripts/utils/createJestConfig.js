0bef52a619bbec3df7fc6a4003a2360a43ec634b
Tokyo <ahmed.tokyo1@gmail.com>
2018-05-11 18:50:19 +0200

    :bug: readd css modules id obj proxy

--- scripts/utils/createJestConfig.js
+++ scripts/utils/createJestConfig.js
@@ -42,6 +42,7 @@ module.exports = (resolve, rootDir, isEjecting) => {
     transformIgnorePatterns: ['[/\\\\]node_modules[/\\\\].+\\.(js|jsx|mjs)$'],
     moduleNameMapper: {
       '^react-native$': 'react-native-web',
+      '^.+\\.module\\.css$': 'identity-obj-proxy',
     },
     moduleFileExtensions: [
       'web.js',
